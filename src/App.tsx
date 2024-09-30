e1890ea3e8389de8d3d7cfb6b027f6c345b8c376
secretariatv <oliver.b25.f@gmail.com>
2024-09-30 22:53:11 +0900

    test google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import axiosInstance from "@utils/axiosUtils";
+
 const App = () => {
-  return <></>;
+  const handleLogin = async () => {
+    await axiosInstance.post(
+      "/auth/login",
+      {
+        email: "oliver.b25.f@gmail.com",
+        password: "123123123",
+      },
+      { withCredentials: true }
+    );
+  };
+
+  const handleGoogleRegister = async () => {
+    window.location.href = `${process.env.BACKEND_URL}/auth/google`;
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleLogin}>
+        On Click
+      </button>
+      <button type="button" onClick={handleGoogleRegister}>
+        Google Auth
+      </button>
+    </>
+  );
 };
 
 export default App;
